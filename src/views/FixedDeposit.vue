a3221ebde4bf15a9c61bf636d2edfad4be1fad48
Mark Holdt <mark@analytics.local>
2019-10-10 08:01:39 +0200

    recommit

---
+++ src/views/FixedDeposit.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <RatesList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RatesList from "@/components/RatesList.vue";
+
+export default {
+  name: "home",
+  components: {
+    RatesList
+  }
+};
+</script>
