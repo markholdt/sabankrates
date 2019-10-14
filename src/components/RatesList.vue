f40a8b525fce6f668a95cce2bae431a8cf97b56b
Mark Holdt <mark@analytics.local>
2019-10-14 07:46:43 +0200

    added

--- src/components/RatesList.vue
+++ src/components/RatesList.vue
@@ -55,17 +55,7 @@
 
             <div class="ui primary button" @click.prevent="getResults(1)">Update</div>
             <br />
-            <div class="ui medium rectangle ad">
-              <!-- Ad Code Goes Here
-  <ins class="adsbygoogle"
-       style="display:inline-block;width:300px;height:250px"
-       data-ad-client="ca-pub-XXXXXXXXXXXXXXXX"
-       data-ad-slot="XXXXXXXXXX"></ins>
-  <script>
-  (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
-              !-->
-            </div>
+            <div class="ui medium rectangle ad">asd</div>
           </div>
         </div>
         <div class="twelve wide column">
